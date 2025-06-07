1051ec1d224ca337fbebda058a32abde83b40a5c
Johan-G27 <jomagri.u@gmail.com>
2025-06-06 21:32:27 -0500

    Actualización

--- classes/output/courseformat/content/section/cmitem.php
+++ classes/output/courseformat/content/section/cmitem.php
@@ -5,6 +5,7 @@ use core_courseformat\output\local\content\section\cmitem as core_cmitem;
 use renderer_base;
 use stdClass;
 use core_tag_tag;
+use context_module;
 
 class cmitem extends core_cmitem {
 
@@ -13,15 +14,21 @@ class cmitem extends core_cmitem {
 
         $data = parent::export_for_template($output);
 
+        $context = context_module::instance($this->mod->id);
+        $is = has_capability('moodle/course:update', $context);
+
         if ($this->format->get_course()->format !== 'alpy') {
             return $data;
         }
 
+
+        if ($is) {
         // Obtener etiquetas del módulo
         $tags = core_tag_tag::get_item_tags('core', 'course_modules', $this->mod->id);
         $data->tags = [];
         foreach ($tags as $tag) {
             $data->tags[] = $tag->rawname;
+        }
         }
 
         return $data;
