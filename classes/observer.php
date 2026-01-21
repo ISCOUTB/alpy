c26531617100898aba2636842578dbde91966b9c
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-20 22:32:04 -0500

    Refactor observer for alpy course format events

--- classes/observer.php
+++ classes/observer.php
@@ -1,74 +1,149 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Event observers used by the weeks course format.
+ * Event observers for the alpy course format.
  *
- * @package format_weeks
- * @copyright 2017 Mark Nelson <markn@moodle.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    format_alpy
+ * @copyright  2026 SAVIO - Sistema de Aprendizaje Virtual Interactivo (UTB)
+ * @author     SAVIO Development Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+namespace format_alpy;
 
-/**
- * Event observer for format_weeks.
- *
- * @package format_weeks
- * @copyright 2017 Mark Nelson <markn@moodle.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class format_weeks_observer {
+use core\event\course_updated;
+use core\event\course_section_created;
+use core\event\course_section_deleted;
+
+class observer {
 
     /**
      * Triggered via \core\event\course_updated event.
      *
-     * @param \core\event\course_updated $event
+     * @param course_updated $event
      */
-    public static function course_updated(\core\event\course_updated $event) {
-        if (class_exists('format_weeks', false)) {
-            // If class format_weeks was never loaded, this is definitely not a course in 'weeks' format.
-            // Course may still be in another format but format_weeks::update_end_date() will check it.
-            format_weeks::update_end_date($event->courseid);
+    public static function course_updated(course_updated $event) {
+        global $CFG;
+        $courseid = (int)$event->courseid;
+        if ($courseid > 0) {
+            require_once($CFG->dirroot . '/course/format/alpy/lib.php');
+            \format_alpy::update_end_date($courseid);
         }
     }
 
     /**
      * Triggered via \core\event\course_section_created event.
      *
-     * @param \core\event\course_section_created $event
+     * @param course_section_created $event
      */
-    public static function course_section_created(\core\event\course_section_created $event) {
-        if (class_exists('format_weeks', false)) {
-            // If class format_weeks was never loaded, this is definitely not a course in 'weeks' format.
-            // Course may still be in another format but format_weeks::update_end_date() will check it.
-            format_weeks::update_end_date($event->courseid);
+    public static function course_section_created(course_section_created $event) {
+        global $CFG;
+        $courseid = (int)$event->courseid;
+        if ($courseid > 0) {
+            require_once($CFG->dirroot . '/course/format/alpy/lib.php');
+            \format_alpy::update_end_date($courseid);
         }
     }
 
     /**
      * Triggered via \core\event\course_section_deleted event.
      *
-     * @param \core\event\course_section_deleted $event
+     * @param course_section_deleted $event
      */
-    public static function course_section_deleted(\core\event\course_section_deleted $event) {
-        if (class_exists('format_weeks', false)) {
-            // If class format_weeks was never loaded, this is definitely not a course in 'weeks' format.
-            // Course may still be in another format but format_weeks::update_end_date() will check it.
-            format_weeks::update_end_date($event->courseid);
+    public static function course_section_deleted(course_section_deleted $event) {
+        global $CFG;
+        $courseid = (int)$event->courseid;
+        if ($courseid > 0) {
+            require_once($CFG->dirroot . '/course/format/alpy/lib.php');
+            \format_alpy::update_end_date($courseid);
+        }
+    }
+
+    /**
+     * Triggered via \core\event\course_created event.
+     * This handles the initialization of Academic Period logic for NEW courses.
+     *
+     * @param \core\event\course_created $event
+     */
+    public static function course_created(\core\event\course_created $event) {
+        global $DB;
+        $courseid = (int)$event->objectid;
+        
+        // SECURITY: Validate courseid
+        if ($courseid <= 0) {
+            return;
+        }
+        
+        $course = $DB->get_record('course', ['id' => $courseid]);
+        if (!$course) {
+            return;
+        }
+
+        // Only proceed if the course format is 'alpy'
+        if ($course->format !== 'alpy') {
+            return;
+        }
+        
+        // We need to check the format options that were just saved
+        // Since $course object might not have complete format options, we fetch them
+        $format = course_get_format($course);
+        $options = $format->get_format_options();
+
+        if (isset($options['setup_mode']) && $options['setup_mode'] == 1) {
+            // Apply Academic Period Logic
+            $current_year = date('Y');
+            $current_month = date('n');
+
+            if ($current_month >= 6) {
+                $start_month = 8; // August
+            } else {
+                $start_month = 2; // February
+            }
+
+            $start_date_str = "first monday of " . date("F", mktime(0, 0, 0, $start_month, 10)) . " $current_year";
+            $start_timestamp = strtotime($start_date_str);
+
+            // 1. Update Start Date
+            $DB->set_field('course', 'startdate', $start_timestamp, ['id' => $courseid]);
+            
+            // 2. Refresh Cache
+            rebuild_course_cache($courseid, true);
+            $course = $DB->get_record('course', ['id' => $courseid]);
+
+            // 3. Create 16 sections
+            // Direct DB update for format options to avoid 'protected method' errors
+            $opt_numsections = $DB->get_record('course_format_options', ['courseid' => $courseid, 'format' => 'alpy', 'name' => 'numsections']);
+            if ($opt_numsections) {
+                $opt_numsections->value = 16;
+                $DB->update_record('course_format_options', $opt_numsections);
+            } else {
+                $DB->insert_record('course_format_options', (object)['courseid' => $courseid, 'format' => 'alpy', 'name' => 'numsections', 'value' => 16, 'sectionid' => 0]);
+            }
+
+            $opt_setupmode = $DB->get_record('course_format_options', ['courseid' => $courseid, 'format' => 'alpy', 'name' => 'setup_mode']);
+            if ($opt_setupmode) {
+                $opt_setupmode->value = 1;
+                $DB->update_record('course_format_options', $opt_setupmode);
+            } else {
+                $DB->insert_record('course_format_options', (object)['courseid' => $courseid, 'format' => 'alpy', 'name' => 'setup_mode', 'value' => 1, 'sectionid' => 0]);
+            }
+            
+            course_create_sections_if_missing($course, range(1, 16));
+
+            // 4. Update end date only if automatic end date is enabled
+            rebuild_course_cache($courseid, true); // Rebuild again to see new sections
+            if (!empty($options['automaticenddate'])) {
+                \format_alpy::update_end_date($courseid);
+            }
+        } else {
+             // Manual Mode Logic: Ensure Monday start
+             if (date('N', $course->startdate) != 1) {
+                $new_start = strtotime('next monday', $course->startdate);
+                $DB->set_field('course', 'startdate', $new_start, ['id' => $courseid]);
+                rebuild_course_cache($courseid, true);
+                if (!empty($options['automaticenddate'])) {
+                    \format_alpy::update_end_date($courseid);
+                }
+             }
         }
     }
 }
