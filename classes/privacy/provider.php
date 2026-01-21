0c4d5705d173900c62bc6abb5016b0b59be735d1
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-20 22:33:13 -0500

    Refactor privacy provider for alpy course format

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,34 +1,18 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Privacy Subsystem implementation for format_weeks.
+ * Privacy provider for the alpy course format.
  *
- * @package    format_weeks
- * @copyright  2018 Carlos Escobedo <carlos@moodle.com>
+ * @package    format_alpy
+ * @copyright  2026 SAVIO - Sistema de Aprendizaje Virtual Interactivo (UTB)
+ * @author     SAVIO Development Team
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-namespace format_weeks\privacy;
-defined('MOODLE_INTERNAL') || die();
-/**
- * Privacy Subsystem for format_weeks implementing null_provider.
- *
- * @copyright  2018 Carlos Escobedo <carlos@moodle.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class provider implements \core_privacy\local\metadata\null_provider {
+
+namespace format_alpy\privacy;
+
+use core_privacy\local\metadata\null_provider;
+
+class provider implements null_provider {
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
@@ -36,6 +20,6 @@ class provider implements \core_privacy\local\metadata\null_provider {
      * @return  string
      */
     public static function get_reason(): string {
-        return 'privacy:metadata';
+        return "privacy:metadata";
     }
 }
