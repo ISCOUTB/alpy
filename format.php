cce776f3267c098964f527f315b23fdf6d6a37e2
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-20 22:25:52 -0500

    Refactor format.php for course format and JS compatibility
    Updated file header information and removed deprecated parameter aliasing.

--- format.php
+++ format.php
@@ -1,31 +1,21 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Weeks course format.  Display the whole course as "weeks" made of modules.
+ * Main display file for the alpy course format.
  *
- * @package format_weeks
- * @copyright 2006 The Open University
- * @author N.D.Freear@open.ac.uk, and others.
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    format_alpy
+ * @copyright  2026 SAVIO - Sistema de Aprendizaje Virtual Interactivo (UTB)
+ * @author     SAVIO Development Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG, $PAGE, $course;
+
+// Validate required variables.
+if (!isset($course) || !is_object($course)) {
+    throw new moodle_exception('invalidcourseid');
+}
 
 require_once($CFG->libdir.'/filelib.php');
 require_once($CFG->libdir.'/completionlib.php');
@@ -33,17 +23,6 @@ require_once($CFG->libdir.'/completionlib.php');
 $format = core_courseformat\base::instance($course);
 $course = $format->get_course();
 $context = context_course::instance($course->id);
-
-// Horrible backwards compatible parameter aliasing..
-if ($week = optional_param('week', 0, PARAM_INT)) {
-    $url = $PAGE->url;
-    $url->param('section', $week);
-    debugging('Outdated week param passed to course/view.php', DEBUG_DEVELOPER);
-    redirect($url);
-}
-// End backwards-compatible aliasing..
-
-$format = course_get_format($course);
 
 // Make sure section 0 is created.
 course_create_sections_if_missing($format->get_course(), 0);
@@ -53,6 +32,22 @@ $renderer = $PAGE->get_renderer('format_alpy');
 if (!is_null($displaysection)) {
    $format->set_section_number($displaysection);
 }
+
+// In Moodle 4.x, we must ensure M.course.format is initialized for legacy JS compatibility.
+// We use a stronger initialization that includes creating the namespace if it doesn't exist.
+$js = "
+if (typeof M.course === 'undefined') { M.course = {}; }
+if (typeof M.course.format === 'undefined') { M.course.format = {}; }
+M.course.format.get_config = function() {
+    return {
+        'container_node': 'ul',
+        'container_class': 'topics',
+        'section_node': 'li',
+        'section_class': 'section'
+    };
+};
+";
+$PAGE->requires->js_init_code($js);
 
 $outputclass = $format->get_output_classname('content');
 $output = new $outputclass($format);
