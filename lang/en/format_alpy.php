d4ca522cc7645ffb6a9b65569fd492e76e64210f
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-20 22:43:44 -0500

    Revise Alpy format plugin metadata
    Updated plugin metadata to reflect accurate descriptions and privacy information.

--- lang/en/format_alpy.php
+++ lang/en/format_alpy.php
@@ -1,40 +1,14 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Strings for component 'format_weeks', language 'en', branch 'MOODLE_20_STABLE'
- *
- * @package   format_weeks
- * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-$string['addsection'] = 'Add week';
 $string['automaticenddate'] = 'Calculate the end date from the number of sections';
 $string['automaticenddate_help'] = 'If enabled, the end date for the course will be automatically calculated from the number of sections and the course start date.';
 $string['currentsection'] = 'Current week';
-$string['formatname'] = 'Alpy format';
-$string['hidefromothers'] = 'Hide';
-$string['page-course-view-alpy'] = 'Any course main page in weekly sections format';
-$string['page-course-view-alpy-x'] = 'Any course page in weekly sections format';
 $string['pluginname'] = 'Alpy';
-$string['plugin_description'] = ' This course format reorders activities according to the student\'s learning style and displays relevant tags.';
-$string['privacy:metadata'] = 'The Weekly sections format plugin does not store any personal data.';
+$string['plugin_description'] = 'This course format reorders activities according to the student\'s learning style and displays relevant tags.';
+$string['privacy:metadata'] = 'The Alpy format plugin does not store any personal data.';
 $string['indentation'] = 'Allow indentation on course page';
 $string['indentation_help'] = 'Allow teachers, and other users with the manage activities capability, to indent items on the course page.';
 $string['section0name'] = 'General';
-$string['sectionname'] = 'Alpy';
-$string['showfromothers'] = 'Show';
+$string['setup_mode'] = 'Date Setup Mode';
+$string['setup_mode_help'] = 'If you select "Academic Period", the system will automatically set the start date (First Monday of February or August) and 16 weeks. Use "Manual" to choose freely.';
+$string['mode_manual'] = 'Manual';
+$string['mode_academic'] = 'Academic Period (Auto)';
