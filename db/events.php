ecc37910d6728fd1bb32604f885235a6de7d7b3f
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-20 22:38:17 -0500

    Update event handlers for alpy course format

--- db/events.php
+++ db/events.php
@@ -1,25 +1,11 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Format weeks event handler definition.
+ * Event handlers for the alpy course format.
  *
- * @package format_weeks
- * @copyright 2017 Mark Nelson <markn@moodle.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    format_alpy
+ * @copyright  2026 SAVIO - Sistema de Aprendizaje Virtual Interactivo (UTB)
+ * @author     SAVIO Development Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
@@ -27,14 +13,18 @@ defined('MOODLE_INTERNAL') || die();
 $observers = array(
     array(
         'eventname'   => '\core\event\course_updated',
-        'callback'    => 'format_weeks_observer::course_updated',
+        'callback'    => '\format_alpy\observer::course_updated',
     ),
     array(
         'eventname'   => '\core\event\course_section_created',
-        'callback'    => 'format_weeks_observer::course_section_created',
+        'callback'    => '\format_alpy\observer::course_section_created',
     ),
     array(
         'eventname'   => '\core\event\course_section_deleted',
-        'callback'    => 'format_weeks_observer::course_section_deleted',
-    )
+        'callback'    => '\format_alpy\observer::course_section_deleted',
+    ),
+    array(
+        'eventname'   => '\core\event\course_created',
+        'callback'    => '\format_alpy\observer::course_created',
+    ),
 );
